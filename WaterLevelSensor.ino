03b08a45c33959a0a1e9cdf9aa6a67ff47c07a2a
Md Ishrak Mashroor <101423584+mdishrak@users.noreply.github.com>
2024-01-23 06:21:15 -0700

    LED allocation

--- WaterLevelSensor.ino
+++ WaterLevelSensor.ino
@@ -24,4 +24,34 @@ void setup() {
   digitalWrite(yellowLED, LOW);
   digitalWrite(greenLED, LOW);
 }
+void loop() {
+  int level = readSensor();
+
+  if (level == 0) {
+    Serial.println("Water Level: Empty");
+    digitalWrite(redLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(greenLED, LOW);
+  }
+  else if (level > 0 && level <= 99) {
+    Serial.println("Water Level: Low");
+    digitalWrite(redLED, LOW);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(greenLED, HIGH);
+  }
+  else if (level >= 100 && level <= 150) { // Corrected the condition here
+    Serial.println("Water Level: Medium");
+    digitalWrite(redLED, LOW);
+    digitalWrite(yellowLED, HIGH);
+    digitalWrite(greenLED, LOW);
+  }
+  else if (level > 155) {
+    Serial.println("Water Level: High");
+    digitalWrite(redLED, HIGH);
+    digitalWrite(yellowLED, LOW);
+    digitalWrite(greenLED, LOW);
+  }
+  delay(1000); // Wait for a second before reading again
+}
+
 
