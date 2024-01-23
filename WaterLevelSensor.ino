8535669afb14d07568e859635e2e1e6d5a7918ce
Md Ishrak Mashroor <101423584+mdishrak@users.noreply.github.com>
2024-01-23 06:20:03 -0700

    Update WaterLevelSensor.ino

--- WaterLevelSensor.ino
+++ WaterLevelSensor.ino
@@ -1,1 +1,16 @@
+// Sensor pins
+#define sensorPower 7
+#define sensorPin A5
 
+// Value for storing water level
+int val = 0;
+
+// Declare pins to which LEDs are connected
+int redLED = 2;
+int yellowLED = 3;
+int greenLED = 4;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensorPower, OUTPUT);
+  digitalWrite(sensorPower, LOW);
